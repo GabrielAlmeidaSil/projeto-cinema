7382e23ab2c16940899d453253c2b141c2ce0c2e
Gabriel Almeida <157758748+GabrielAlmeidaSil@users.noreply.github.com>
2025-04-17 19:13:29 -0300

    Botões menu-mobile

--- js/script.js
+++ js/script.js
@@ -17,3 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const header = document.querySelector('header');
