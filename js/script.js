d87e4e8bd9e345604c71058d5f2f87cd7e25cc2a
Gabriel Almeida <157758748+GabrielAlmeidaSil@users.noreply.github.com>
2025-04-17 18:39:07 -0300

    Nova animação no menu-mobile

--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   const menuIcon = document.getElementById('menuIcon');
-  const icon = menuIcon.querySelector('i');
   const mobileMenu = document.getElementById('mobileMenu');
   const mobileLinks = mobileMenu.querySelectorAll('a');
 
   menuIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
+    menuIcon.classList.toggle('menu-opened'); // <- isso aciona a animação
 
-    if (mobileMenu.classList.contains('active')) {
-      icon.classList.remove('bi-list');
-      icon.classList.add('bi-x');
-    } else {
-      icon.classList.remove('bi-x');
-      icon.classList.add('bi-list');
-    }
+    // Remove "bi-list" e "bi-x" desnecessários (agora é feito com CSS)
   });
 
   mobileLinks.forEach(link => {
     link.addEventListener('click', () => {
       mobileMenu.classList.remove('active');
-      icon.classList.remove('bi-x');
-      icon.classList.add('bi-list');
+      menuIcon.classList.remove('menu-opened');
     });
   });
 });
